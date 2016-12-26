be184af25510d229e1287abbe242398655a596a3
Hallez <jbhallez@aol.com>
2016-12-26 12:39:35 +0100

    (26/12/16 Bug Droit d'accès et bug de fin de minutetie Ok) Lance une minuterie de Jarvis pour X temps.
    You: Lance la minuterie
    Jarvis: Je lance la minuterie pour combien de temps ?
    You: 4 minutes
    Jarvis:  Ok c'est parti pour 4 minutes
    
    NEWS:
    You: Efface la minuterie
    Jarvis:  J'efface toutes les minuteries en cours...

--- fr/functions.sh
+++ fr/functions.sh
@@ -36,17 +36,18 @@ fi
 }
 
 
-
 jv_pg_ct_MinuterieOk () {
 if [ -e "./plugins/jarvis-minuterie/port.txt" ]; then
-else 
-sudo chmod +x ./plugins/jarvis-minuterie/minuterie-deb.sh
-sudo chmod +x ./plugins/jarvis-minuterie/minuterie-fin.sh
-
-fi
-
 echo "$AddressJarvis" > plugins/jarvis-minuterie/port.txt
 ./plugins/jarvis-minuterie/minuterie-deb.sh $TempsminOusec
+
+else
+sudo chmod +x ./plugins/jarvis-minuterie/minuterie-deb.sh
+sudo chmod +x ./plugins/jarvis-minuterie/minuterie-fin.sh
+echo "$AddressJarvis" > plugins/jarvis-minuterie/port.txt
+./plugins/jarvis-minuterie/minuterie-deb.sh $TempsminOusec
+fi
+
 }
 
 
