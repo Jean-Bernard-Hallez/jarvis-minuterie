77d1d39a3e71a28284e89fa5defb09bfd626773d
Hallez <jbhallez@aol.com>
2017-05-08 15:40:18 +0200

    (02/05/17 Correction suite modif du "plugins_installed") (26/12/16 Bug Droit d'accès et bug de fin de minutetie Ok) Lance une minuterie de J
    You: Lance la minuterie
    Jarvis: Je lance la minuterie pour combien de temps ?
    You: 4 minutes
    Jarvis:  Ok c'est parti pour 4 minutes
    
    NEWS:
    You: Efface la minuterie
    Jarvis:  J'efface toutes les minuteries en cours..

--- minuterie-deb.sh
+++ minuterie-deb.sh
@@ -1,1 +1,1 @@
-echo "/home/pi/jarvis/plugins/jarvis-minuterie/minuterie-fin.sh" | at now+$* minutes &>/dev/null
+echo "/home/pi/jarvis/plugins_installed/jarvis-minuterie/minuterie-fin.sh" | at now+$* minutes &>/dev/null
--- minuterie-fin.sh
+++ minuterie-fin.sh
@@ -1,3 +1,4 @@
-port=`$(echo cat /home/pi/jarvis/plugins/jarvis-minuterie/port.txt)`
+port=`$(echo cat /home/pi/jarvis/plugins_installed/jarvis-minuterie/port.txt)`
 curl "$port?say=Fin%20de%20la%20minuterie%20programmé%20par%20Jarvis"
-mpg321 /home/pi/jarvis/plugins/jarvis-minuterie/bruit7.mp3
+mpg321 /home/pi/jarvis/plugins_installed/jarvis-minuterie/bruit7.mp3
+/home/pi/jarvis/jarvis.sh "-s Fin de la minuterie"
